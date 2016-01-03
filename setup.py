f1e1a754b874ec411d48a31b348915653fb8ecde
Christopher Qian <cqian19@hotmail.com>
2016-01-02 16:06:29 -0800

    Working cx_freeze installer build with shortcut

--- setup.py
+++ setup.py
@@ -8,11 +8,35 @@ includes = ['lxml.etree','lxml._elementpath']
 include_files = ['images/', 'guifiles/', 'config.ini', (requests.certs.where(),'cacert.pem')]
 packages = ['inspect', 'os', 'requests', 'PySide.QtCore', 'PySide.QtGui', 'RbxAPI', ]
 
+shortcut_table = [
+    ("DesktopShortcut",        # Shortcut
+     "DesktopFolder",          # Directory_
+     "Valk TC Bot",           # Name
+     "TARGETDIR",              # Component_
+     "[TARGETDIR]ValkTCBot.exe",# Target
+     None,                     # Arguments
+     None,                     # Description
+     None,                     # Hotkey
+     None,                     # Icon
+     None,                     # IconIndex
+     None,                     # ShowCmd
+     'TARGETDIR'               # WkDir
+     )
+]
+
+msi_data = dict(
+    Shortcut=shortcut_table
+)
+
+bdist_msi_options = dict(
+    data=msi_data
+)
 
 buildOptions = dict(
     include_files=include_files,
     packages=packages,
-    includes=includes
+    includes=includes,
+    append_script_to_exe=True,
 )
 
 executables = [
@@ -21,8 +45,6 @@ executables = [
         targetName='ValkTCBot.exe',
         base="Win32GUI", # THIS ONE IS IMPORTANT FOR GUI APPLICATION
         icon='images/bot_icon.ico',
-        shortcutName= "Valk TC Bot",
-        shortcutDir="DesktopFolder",
     )
 ]
 
@@ -30,6 +52,6 @@ setup(
     name="Valk TC Bot",
     version="1.0",
     description="A trade currency bot for Roblox.",
-    options=dict(build_exe=buildOptions),
+    options=dict(build_exe=buildOptions,bdist_msi=bdist_msi_options),
     executables=executables
 )
